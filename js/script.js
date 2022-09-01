bf2aebc085698fae213a9333850b902cdc776198
Emmanuel <emmanwel@gmail.com>
2022-09-01 01:27:01 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btnAgregar = document.getElementById("btnAgregar");
+const itemCount = document.getElementById("itemCount");
+const itemName = document.getElementById("itemName");
+
+let count;
+
+btnAgregar.addEventListener("click", () => {});
