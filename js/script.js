34497682b4bef5171fe44f39ed10104600c7f36f
Emmanuel <emmanwel@gmail.com>
2022-09-03 21:11:06 -0300

    magia creada

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-const btnAgregar = document.getElementById("btnAgregar");
-const itemCount = document.getElementById("itemCount");
-const itemName = document.getElementById("itemName");
+const fabBtn = document.getElementById("fabBtn");
+const closeBtn = document.getElementById("closeBtn");
+const addPopup = document.getElementById("addPopup");
 
-let count;
+//Con la siguiente función hago la mágia de mostrar u ocultar lo que quiero segun como se visualice la app
+function magia(x) {
+  if (laMagia.matches) {
+    fabBtn.classList.remove("d-none");
+    closeBtn.classList.remove("d-none");
+    addPopup.classList.add("d-none");
+  } else {
+    fabBtn.classList.add("d-none");
+    closeBtn.classList.add("d-none");
+    addPopup.classList.remove("d-none");
+  }
+}
 
-btnAgregar.addEventListener("click", () => {});
+var laMagia = window.matchMedia("(max-width: 768px)");
+magia(laMagia);
+laMagia.addListener(magia);
